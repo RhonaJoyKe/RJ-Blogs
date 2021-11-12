c229d4c5499138eb04069bdec4e5e33d32b47ddd
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-11-12 21:50:10 +0300

    adds a comment route

--- app/main/views.py
+++ app/main/views.py
@@ -30,3 +30,46 @@ def update_profile(uname):
         db.session.commit()
         return redirect(url_for('.profile',uname=user.username))
     return render_template('profile/update.html',form =form)
+@main.route('/createblog', methods=['GET', 'POST'])
+@login_required
+def new_blog():
+    blog_form = FormBlog()
+    if blog_form.validate_on_submit():
+        title = blog_form.title.data
+        category = blog_form.category.data
+        content = blog_form.content.data
+        new_blog= Blogs(title=title, category=category, content=content)
+        new_blog.save_blogs
+        
+        return redirect(url_for('main.index'))
+    return render_template('blog.html', blog_form = blog_form)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+@main.route('/comments/<pitch_id>', methods=['GET', 'POST'])
+@login_required
+def comments(blog_id):
+    comments = Comment.query.filter_by(blog_id=blog_id).all()
+    blog = Blogs.query.get(blog_id)
+    form = CommentForm()
+    if blog is None:
+        abort(404)
+    if form.validate_on_submit():
+            comment = Comment(
+            content=form.content.data,
+            pitch_id=pitch_id,
+            user_id=current_user.id
+        )
+            db.session.add(comment)
+            db.session.commit()
+            form.content.data = ''
+            flash('Your comment has been posted successfully!')
+    return render_template('comments.html',pitches= pitch, comment=comments, form = form)
