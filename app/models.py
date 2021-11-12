e610140c134dc2810c27eea0e943a63754ac2d31
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-11-12 13:25:56 +0300

    includes the init .py code

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 # user table
 class User(UserMixin, db.Model):  
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255))
     email = db.Column(db.String(255), unique=True, index=True)
