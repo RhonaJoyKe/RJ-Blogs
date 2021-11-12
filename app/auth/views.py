adddcd12db31f6dffb0231276d626a7646e2724e
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-11-12 13:50:36 +0300

    adds registration form and login form

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,7 @@ from . import auth
 from flask import render_template,redirect,url_for, flash,request
 from ..models import User
 from ..email import mail_message
-from .forms import LoginForm,RegistrationForm
+from .forms import RegistrationForm,LoginForm
 from flask_login import login_user,logout_user,login_required
 from .. import db
 # register user
